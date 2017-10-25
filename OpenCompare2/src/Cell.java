8317ed713daa10bc02501d5a6fc41d78f75a7803
quentineono <quentineono@dy085028.etu.univ-rennes1.fr>
2017-10-25 09:42:59 +0200

    ajout des getters and setters

--- OpenCompare2/src/Cell.java
+++ OpenCompare2/src/Cell.java
@@ -4,4 +4,36 @@ public class Cell {
 	private boolean isPartial;
 	private String unit;
 	private String value;
+	public String getFeatureId() {
+		return featureId;
+	}
+	public void setFeatureId(String featureId) {
+		this.featureId = featureId;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public boolean isPartial() {
+		return isPartial;
+	}
+	public void setPartial(boolean isPartial) {
+		this.isPartial = isPartial;
+	}
+	public String getUnit() {
+		return unit;
+	}
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	
 }
