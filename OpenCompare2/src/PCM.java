f082032e4a9e4d7b82aaaf39452ba9b1c95f4265
Quentin Eono <quentineono@MacBook-Pro-de-Quentin.local>
2017-10-26 18:49:50 +0200

    ajout constructeur pcm

--- OpenCompare2/src/PCM.java
+++ OpenCompare2/src/PCM.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class PCM {
-	private String _id;
+	private String id;
 	private String name;
 	private int featureIdGen;
 	private int productIdGen;
@@ -15,73 +15,88 @@ public class PCM {
 	private String primaryFeatureId;
 	private Collection<Feature> features;
 	private Collection<Product> products;
+	
+	public PCM (String _id, String _name, int _featureIdGen, int _productIdGen, String _description, String _license,
+				String _source, String _author, String _primaryFeatureId, Collection<Feature> _features, Collection<Product> _products ){
+		
+		this.id = _id;
+		this.name = _name;
+		this.featureIdGen = _featureIdGen;
+		this.productIdGen = _productIdGen;
+		this.description = _description;
+		this.license = _license;
+		this.source = _source;
+		this.author = _author;
+		this.primaryFeatureId = _primaryFeatureId;
+		this.features = _features;
+		this.products = _products;
+		
+	}
 	public String get_id() {
-		return _id;
+		return id;
 	}
 	public void set_id(String _id) {
-		this._id = _id;
+		this.id = _id;
 	}
 	public String getName() {
 		return name;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setName(String _name) {
+		this.name = _name;
 	}
 	public int getFeatureIdGen() {
 		return featureIdGen;
 	}
-	public void setFeatureIdGen(int featureIdGen) {
-		this.featureIdGen = featureIdGen;
+	public void setFeatureIdGen(int _featureIdGen) {
+		this.featureIdGen = _featureIdGen;
 	}
 	public int getProductIdGen() {
 		return productIdGen;
 	}
-	public void setProductIdGen(int productIdGen) {
-		this.productIdGen = productIdGen;
+	public void setProductIdGen(int _productIdGen) {
+		this.productIdGen = _productIdGen;
 	}
 	public String getDescription() {
 		return description;
 	}
-	public void setDescription(String description) {
-		this.description = description;
+	public void setDescription(String _description) {
+		this.description = _description;
 	}
 	public String getLicense() {
 		return license;
 	}
-	public void setLicense(String license) {
-		this.license = license;
+	public void setLicense(String _license) {
+		this.license = _license;
 	}
 	public String getSource() {
 		return source;
 	}
-	public void setSource(String source) {
-		this.source = source;
+	public void setSource(String _source) {
+		this.source = _source;
 	}
 	public String getAuthor() {
 		return author;
 	}
-	public void setAuthor(String author) {
-		this.author = author;
+	public void setAuthor(String _author) {
+		this.author = _author;
 	}
 	public String getPrimaryFeatureId() {
 		return primaryFeatureId;
 	}
-	public void setPrimaryFeatureId(String primaryFeatureIyd) {
-		this.primaryFeatureId = primaryFeatureId;
+	public void setPrimaryFeatureId(String _primaryFeatureId) {
+		this.primaryFeatureId = _primaryFeatureId;
 	}
 	public Collection<Feature> getFeatures() {
 		return new ArrayList<Feature>(features);
 	}
-	public void setFeatures(Collection<Feature> features) {
-		this.features = features;
+	public void setFeatures(Collection<Feature> _features) {
+		this.features = _features;
 	}
 	public Collection<Product> getProducts() {
 		return new ArrayList<Product>(products);
 	}
-	public void setProducts(Collection<Product> products) {
-		this.products = products;
+	public void setProducts(Collection<Product> _products) {
+		this.products = _products;
 	}
-public void testCommit() {
-		return;
-	}
+
 }
