62a8c4737d70e6b29af2395189df96a20d36cb78
17010063 <17010063>
2018-01-10 13:48:02 +0100

    demo

--- src/test/TestPCM.java
+++ src/test/TestPCM.java
@@ -13,31 +13,32 @@ public class TestPCM {
 	@Test
 	public void test() throws Exception {
 		String jsonTxt = main.Execution.askImportMethod();
-        
-        // Transformation du JSON contenu dans jsonTxt en un objet de type PCM
-        PCMParser parser1 = new PCMParser(jsonTxt);
-        parser1.transformToJavaObject();
-        PCM pcm = parser1.getPCMJavaObject();
-        
-        if (pcm != null) {
-        	System.out.println("La PCM n'est pas nulle, on peut donc poursuivre le test");
-        	
-        	PCM compareTo = pcm;
-        	PCMParser parser2;
-        	
-        	int nbTests = 10;
-        	
-        	// Pour s'assurer du bon fonctionnement de l'API, on réalise nbTests fois l'opération suivante :
-        	for (int i = 0; i < nbTests; i++) {
-        		// On récupère le JSON correspondant à l'objet compareTo et on le transforme en objet PCM
-	        	parser2 = new PCMParser(compareTo.toJSONObject().toJSONString());
-		    	parser2.transformToJavaObject();
-		    	// On affecte à compareTo la nouvelle PCM obtenue
-		    	compareTo = parser2.getPCMJavaObject();
-		    	
-		    	// On compare l'objet compareTo à la PCM d'origine
-		    	assertTrue(Objects.equals(pcm, compareTo));
-        	}
-        }
+
+		// Transformation du JSON contenu dans jsonTxt en un objet de type PCM
+		PCMParser parser1 = new PCMParser(jsonTxt);
+		parser1.transformToJavaObject();
+		PCM pcm = parser1.getPCMJavaObject();
+
+		if (pcm != null) {
+			System.out.println("La PCM n'est pas nulle, on peut donc poursuivre le test");
+
+			PCM compareTo = pcm;
+			PCMParser parser2;
+
+			int nbTests = 10;
+
+			// Pour s'assurer du bon fonctionnement de l'API, on réalise nbTests fois l'opération suivante :
+			for (int i = 0; i < nbTests; i++) {
+				// On récupère le JSON correspondant à l'objet compareTo et on le transforme en objet PCM
+				parser2 = new PCMParser(compareTo.toJSONObject().toJSONString());
+				parser2.transformToJavaObject();
+				// On affecte à compareTo la nouvelle PCM obtenue
+				compareTo = parser2.getPCMJavaObject();
+
+				// On compare l'objet compareTo à la PCM d'origine
+				assertTrue(Objects.equals(pcm, compareTo));
+			}
+		}
 	}
+	
 }
